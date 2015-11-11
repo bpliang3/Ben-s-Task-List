c551b869453f45dc5ffef3892a663ab5b5463f73
Ben Liang <bpliang3@illinois.edu>
2015-11-11 01:43:26 -0600

    Initial Commit

---
+++ Ben's Task List/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Ben's Task List
+//
+//  Created by Ben Liang on 11/11/15.
+//  Copyright (c) 2015 Ben Liang. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
